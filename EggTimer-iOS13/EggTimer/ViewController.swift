4194bcbafa49fe4ecd78c006dd26509335af6a1c
quentinthomas <q1330376@gmail.com>
2021-10-05 16:39:49 -0500

    Add files via upload

---
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = (sender.currentTitle)
+        
+        switch hardness {
+        case "Soft":
+            print(softTime)
+        case "Medium":
+            print(mediumTime)
+        case "Hard":
+            print(hardTime)
+        
+        default:
+            <#code#>
+        }
+    }
+    
+
+
+}
